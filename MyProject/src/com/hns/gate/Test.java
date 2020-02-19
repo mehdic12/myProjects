158a0c9f3f57dd6c24c06cd8ac4e2f90f76f4cb3
Administrateur <Administrateur@DMZ14V12A-LIBSV>
2020-02-19 14:54:25 +0100

    Commit

--- MyProject/src/com/hns/gate/Test.java
+++ MyProject/src/com/hns/gate/Test.java
@@ -2,6 +2,7 @@ package com.hns.gate;
 
 public class Test {
   public static void main(String[] args) {
-	System.out.println("Hello Word......");
+	System.out.println("Modification en vue de mise a jour de Git");
 }
 }
+  
