2ac65fb87f081d55b4183c915d719f14faf4ffff
Administrateur <Administrateur@DMZ14V12A-LIBSV>
2020-02-19 15:05:55 +0100

    test

--- MyProject/src/com/hns/gate/Test.java
+++ MyProject/src/com/hns/gate/Test.java
@@ -2,7 +2,7 @@ package com.hns.gate;
 
 public class Test {
   public static void main(String[] args) {
-	System.out.println("Modification en vue de mise a jour de Git");
+	System.out.println("Je modifie dans branche master....");
 }
 }
   
